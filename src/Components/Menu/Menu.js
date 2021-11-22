b2dca0594d43f76009e0fcd6c08cc78d26dd0c5f
alessandro <alessandrocappiello22@gmail.com>
2021-11-22 10:33:39 +0100

    correzione warning che impedivano il deployment

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -3,8 +3,9 @@ import * as React from "react";
 export const Menu = () => {
   return (
     <div className="Menu">
-        <a href="">Visualizza Repos</a>
-        <a href="">Nuovo Utente</a>
+        <button>Visualizza Repos</button>
+         - 
+        <button>Nuovo Utente</button>
       {" "}
     </div>
   );
